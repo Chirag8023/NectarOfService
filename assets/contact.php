2da2b65c1cb70926162b2fa34d9e44869c6c60f3
Chirag Arora <chirag8023@gmail.com>
2024-04-05 07:19:09 -0700

    contact us css shift to main css file

--- assets/contact.php
+++ assets/contact.php
@@ -1,41 +1,9 @@
     <link rel="stylesheet" href="http://localhost/NectarOfService/assets/main_style.css">
-    <style>
-/* Grid container */
-.grid-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 40px;
-    padding: 20px;
-}
-
-/* Left content */
-.left-content {
-    font-family: 'inter';
-    margin-left: 1rem;
-    font-size: 30px;
-    color: #000;
-    margin-top: -0.5rem;
-}
-
-
-.right-content {
-    background-color: #fff;
-    font-size: 20px;
-    font-family: 'inter';
-    font-weight: 600;
-    margin-right: 1rem;
-    border-radius: 1px;
-    border: 3px solid black;
-    box-shadow: 7px 7px rgba(0, 0, 0);
-    padding: 20px;
-    margin-bottom: 2rem;
-}
-    </style>
     <title>Contact Us</title>
 
     <body class="body">
         <?php include 'header.php'; ?>
-        <div class="grid-container" style="padding-top: 4.5rem;">
+        <div class="content-grid-container" style="padding-top: 4.5rem;">
             <div class="left-content">
                 <h1>Contact us</h1>
                 <p>
