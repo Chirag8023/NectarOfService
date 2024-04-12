582e0c31f04f9de0b7f4ad9ddcc62598c6d410d2
Chirag Arora <chirag8023@gmail.com>
2024-04-12 15:22:01 +0530

    Update 404.php

--- assets/404.php
+++ assets/404.php
@@ -3,10 +3,10 @@
 
     <body class="body">
         <?php include 'header.php'; ?>
-        <div style="margin-top:7rem; text-align:center; font-size:30px;">
+        <div style="margin-top:6rem; text-align:center; font-size:30px;">
+            <p style="font-size:10rem;margin:0; filter:saturate(150%);">&#128543;</p>
             <h1>404 - Not Found</h1>
             <p>Look what did you do!<br>The website is now broken. </p>
-            <p style="font-size:100px;margin:0;">&#128543;</p>
             <p>Go back to <a href="http://localhost/NectarOfService/index.php">home</a></p>
         </div>
 
