57d491d8fe897e0475613b1ea3283b1b2cfccfea
Chirag Arora <chirag8023@gmail.com>
2024-04-13 13:02:31 +0530

    css centralization and php beautifying

--- assets/scripts/get_ongoing_campaigns.php
+++ assets/scripts/get_ongoing_campaigns.php
@@ -1,24 +1,28 @@
 <?php
-include 'dbconnect.php';
+include "dbconnect.php";
 
-$ongoingCampaigns = "SELECT * FROM fundraising_campaigns WHERE current_amount < goal ORDER BY id DESC";
+$ongoingCampaigns =
+    "SELECT * FROM fundraising_campaigns WHERE current_amount < goal ORDER BY id DESC";
 $ongoingResult = $conn->query($ongoingCampaigns);
 
 $ongoingCampaignData = [];
 
 if ($ongoingResult->num_rows > 0) {
-    while($row = $ongoingResult->fetch_assoc()) {
+    while ($row = $ongoingResult->fetch_assoc()) {
         $percentage = ($row["current_amount"] / $row["goal"]) * 100;
-        
+
         // Storing campaign data in an array
         $campaign = [
-            'title' => $row["title"],
-            'description' => strlen($row['description']) > 200 ? substr($row['description'], 0, 200) . '...' : $row['description'].'<br><br>',
-            'goal' => $row["goal"],
-            'current_amount' => $row["current_amount"],
-            'percentage' => round($percentage, 2),
-            'image_path' => '../assets/images/' . $row['image_filename'],
-            'id' => $row['id']
+            "title" => $row["title"],
+            "description" =>
+                strlen($row["description"]) > 200
+                    ? substr($row["description"], 0, 200) . "..."
+                    : $row["description"] . "<br><br>",
+            "goal" => $row["goal"],
+            "current_amount" => $row["current_amount"],
+            "percentage" => round($percentage, 2),
+            "image_path" => "../assets/images/" . $row["image_filename"],
+            "id" => $row["id"],
         ];
         $ongoingCampaignData[] = $campaign;
     }
@@ -28,11 +32,10 @@ if ($ongoingResult->num_rows > 0) {
 ?>
 
 <?php foreach ($ongoingCampaignData as $campaign): ?>
-<div class="fundraiser-donation-data" style="display: flex;">
-    <?php if (file_exists($campaign['image_path'])): ?>
-    <div class="image-container" style="flex: 30%; height:19rem;">
-        <img src="<?php echo $campaign['image_path']; ?>"
-            style="object-fit: cover;width:100%;height:100% !important; margin-left:-1rem;border:3px solid black;"
+<div class="fundraiser-donation-data">
+    <?php if (file_exists($campaign["image_path"])): ?>
+    <div class="image-container">
+        <img src="<?php echo $campaign["image_path"]; ?>"
             alt="Campaign Image">
     </div>
     <?php endif; ?>
@@ -42,14 +45,20 @@ if ($ongoingResult->num_rows > 0) {
         <p>Goal: ₹ <?php echo $campaign["goal"]; ?></p>
         <p>Current Amount: ₹ <?php echo $campaign["current_amount"]; ?></p>
         <div class="progress">
-            <div class="progress-bar" style="width: <?php echo min(100, $campaign['percentage']); ?>%;"></div>
-            <div class="progress-text"><?php echo $campaign['percentage']; ?>%</div>
+            <div class="progress-bar" style="width: <?php echo min(
+                100,
+                $campaign["percentage"]
+            ); ?>%;"></div>
+            <div class="progress-text"><?php echo $campaign[
+                "percentage"
+            ]; ?>%</div>
         </div>
-        <?php if ($campaign['percentage'] >= 100): ?>
+        <?php if ($campaign["percentage"] >= 100): ?>
         <p>This campaign has ended.</p>
         <?php else: ?>
-        <a href="http://localhost/nectarofservice/fundraiser/donate.php?campaign_id=<?php echo $campaign['id']; ?>"><button
-                class="header-button">Donate</button></a>
+        <a href="http://localhost/nectarofservice/fundraiser/donate.php?campaign_id=<?php echo $campaign[
+            "id"
+        ]; ?>"><button class="header-button">Donate</button></a>
         <?php endif; ?>
     </div>
 </div>
