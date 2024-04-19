3fe3ddce100fd84864ae024d1be35f40f37f32da
Chirag Arora <chirag8023@gmail.com>
2024-04-19 12:17:02 +0530

    minor design adjustments

--- social_buzz/social_buzz.php
+++ social_buzz/social_buzz.php
@@ -25,7 +25,7 @@
     </div>
     
     <div id="social-buzz-middle">
-        <h1 style="text-align:left;">Looking for <span style="color:#ef0028">Volunteer Opportunities ?</span> anytime check out our Volunteer Hub.</h1>
+        <h1 style="text-align:left;">Looking for <span style="color:#ef0028">Volunteer Opportunities ?</span> Check out our Volunteer Hub. 👇</h1>
     </div>
 
     <div style="display: flex; justify-content: center; margin:4rem 0;">
