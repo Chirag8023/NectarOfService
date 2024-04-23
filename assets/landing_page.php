b08efdda02ff54d1d133849f273b5530faa82a00
Chirag Arora <chirag8023@gmail.com>
2024-04-23 21:20:23 +0530

    main heading added on landing page

--- assets/landing_page.php
+++ assets/landing_page.php
@@ -22,13 +22,27 @@ margin-right:-0.5rem; margin-left:-1%; padding-left: 0.8rem; border-bottom: 2px 
             </div>
         </div>
     </div>
-    <div class="backlines">
-        <p>ate inspire contribute donate inspire contribute<br>ontribute donate inspire contribute</p>
-    </div>
-    <div style="margin-bottom:1rem;">
 
+    <div class="backlines">
+        <p>
+            ate inspire contribute donate inspire contribute
+        </p>
+    </div>
+
+    <h1 style="display: flex; justify-content: center; font-family: 'bungee'; font-weight: 100; text-shadow: 0rem 0.2rem #ffc900; font-size: 3.5rem;">
+        Shape the world through service
+    </h1>
+
+    <div class="backlines">
+        <p>
+            spire contribute donate inspire contribute donate
+        </p>
+    </div>
+
+    <div style="margin-bottom:1rem;">
         <?php include 'terminal_stats/terminal_stats.php'; ?>
     </div>
+
     <?php include 'newsletter/newsletter.php'; ?>
     <?php include 'footer.php'; ?>
 </body>
